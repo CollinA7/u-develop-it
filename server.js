48f20c57450ea79cf3da514df156d99e0809828c
Cayean <collinayala@gmail.com>
2022-09-24 16:00:55 -0500

    created db mysql function

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,6 +7,22 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your mySQL username
+        user: 'root',
+        // your MySQL password
+        password: 'jfuai9-iqyxP-prbTy',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 //Default response fo any other request(Not found)
 app.use((req, res) => {
